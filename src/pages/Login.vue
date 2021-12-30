c7188fa19c2b55d5ae65e5d12a810dfae575da0b
allenwalker2015 <00053812@uca.edu.sv>
2021-12-30 08:25:16 -0600

    Hotfix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container w-80 mx-auto my-20 px-6 py-6 shadow rounded-md">
+  <div class="bg-white container w-80 mx-auto my-20 px-6 py-6 shadow rounded-md">
 
       <h1 class="text-blue-600 font-bold font-sans text-4xl text-center">Login</h1>
-      <div class="h-0.5 bg-gray-200 w-36 mx-auto mt-2.5"></div>
+      <div class="h-0.5 bg-gray-300 w-36 mx-auto mt-2.5"></div>
     <form action="" method="post">
         <div class="flex flex-col my-5">
             <label class="my-2" for="uname">Username</label>
