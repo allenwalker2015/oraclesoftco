34d4d4f3c30b531ab9f2fea3d139de8f76570099
allenwalker2015 <00053812@uca.edu.sv>
2021-12-30 08:32:55 -0600

    Hotfix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white container w-80 mx-auto my-20 px-6 py-6 shadow rounded-md">
+  <div class="container w-80 mx-auto my-20 px-6 py-6 shadow rounded-md">
 
       <h1 class="text-blue-600 font-bold font-sans text-4xl text-center">Login</h1>
       <div class="h-0.5 bg-gray-300 w-36 mx-auto mt-2.5"></div>
